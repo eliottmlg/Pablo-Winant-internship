893647b6f7c3cd556e549cc98a19699c029405ee
Eliott Von-Pine <eliott.vonpine@gmail.com>
2022-04-21 14:32:48 +0200

    putting all package files in internship-pablo

--- julia_training/dolo/launcher.jl
+++ julia_training/dolo/launcher.jl
@@ -1,13 +1,15 @@
 
+
+
 using Pkg
 Pkg.add("Optim")
 using Dolo
 using Optim
-filename = "C:/Users/t480/GitHub/Pablo-Winant-internship/julia_training/dolo/consumption_savings.yaml"
+filename = "C:/Users/t480/GitHub/Pablo-Winant-internship/julia_training/dolo/consumption_savings.yaml" 
 readlines(filename)
 model = yaml_import(filename)
 residuals(model)
 Base.show(model)
 dr_pert = perturb(model)
-dr_global = time_iteration(model)
+dr_global = time_iteration(model) 
     
