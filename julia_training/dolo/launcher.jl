0a3c92cfeec95b7107218213372b230fa9e650f7
Eliott Von-Pine <eliott.vonpine@gmail.com>
2022-04-21 18:38:42 +0200

    d

--- julia_training/dolo/launcher.jl
+++ julia_training/dolo/launcher.jl
@@ -1,6 +1,5 @@
-
-# using Pkg
-#Pkg.add("Optim")
+Pkg.add("Optim")
+using Pkg
 using Dolo
 
 filename = "C:/Users/t480/GitHub/Pablo-Winant-internship/julia_training/dolo/consumption_savings.yaml"
@@ -10,5 +9,5 @@ residuals(model)
 Base.show(model)
 dr_pert = perturb(model)
 dr_global = time_iteration(model)
-    
+
 
