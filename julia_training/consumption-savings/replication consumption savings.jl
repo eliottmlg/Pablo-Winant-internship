087dc586a0a9a83e8302e9b02a8c79ed94e0ea08
Eliott Von-Pine <eliott.vonpine@gmail.com>
2022-05-05 14:19:14 +0200

    rep

--- julia_training/consumption-savings/replication consumption savings.jl
+++ julia_training/consumption-savings/replication consumption savings.jl
@@ -16,6 +16,8 @@ Pkg.add("NLsolve")
 Pkg.add("Random")
 Pkg.add("Parameters")
 Pkg.add("LaTeXStrings")
+Pkg.add("BenchmarkTools")
+Pkg.add("Roots")    
 using LinearAlgebra, Statistics
 using Plots, QuantEcon, Interpolations, NLsolve, Optim, Random, Parameters, LaTeXStrings
 using Optim: maximum, maximizer 
@@ -164,19 +166,10 @@ plot!(plt, legend = :bottomright)
 
 
 ############ Time iteration
-
-
-
-
-
-
-
-
-
-
-
-
-
+using LinearAlgebra, Statistics
+using BenchmarkTools, Interpolations, LaTeXStrings, Parameters, Plots, QuantEcon, Roots
+using Optim, Random
+using BenchmarkTools, Interpolations, Parameters, Plots, QuantEcon, Roots
 
 function K!(Kg, g, grid, β, ∂u∂c, f, f′, shocks)
     # This function requires the container of the output value as argument Kg
@@ -198,3 +191,106 @@ function K!(Kg, g, grid, β, ∂u∂c, f, f′, shocks)
     # The following function does NOT require the container of the output value as argument
     K(g, grid, β, ∂u∂c, f, f′, shocks) =
         K!(similar(g), g, grid, β, ∂u∂c, f, f′, shocks)
+
+isoelastic(c, γ) = isone(γ) ? log(c) : (c^(1 - γ) - 1) / (1 - γ)
+Model = @with_kw (        α = 0.36,                            # Productivity parameter
+                          β = 0.99,                            # Discount factor
+                          γ = 1,                             # Risk aversion
+                          μ = 0.0,                             # First parameter in lognorm(μ, σ)
+                          s = 0.1,                             # Second parameter in lognorm(μ, σ)
+                          grid = range(1e-6, 4, length = 200), # Grid
+                          grid_min = 1e-6,                     # Smallest grid point
+                          grid_max = 4.0,                      # Largest grid point
+                          grid_size = 200,                     # Number of grid points
+                          u = (c, γ = γ) -> isoelastic(c, γ),  # utility function
+                          ∂u∂c = c -> c^(-γ),                  # u′
+                          f = k -> k^α,                        # production function
+                          f′ = k -> α * k^(α - 1),             # f′
+                          )
+
+
+m = Model();
+using Random
+Random.seed!(42) # for reproducible results.
+
+shock_size = 250 # number of shock draws in Monte Carlo integral
+shocks = collect(exp.(m.μ .+ m.s * randn(shock_size))); # generate shocks
+
+function verify_true_policy(m, shocks, c_star)
+    # compute (Kc_star)
+    @unpack grid, β, ∂u∂c, f, f′ = m
+    c_star_new = K(c_star, grid, β, ∂u∂c, f, f′, shocks)
+
+    # plot c_star and Kc_star
+    plot(grid, c_star, label = L"optimal policy $c^*$")
+    plot!(grid, c_star_new, label = L"Kc^*")
+    plot!(legend = :topleft)
+end
+
+c_star = (1 - m.α * m.β) * m.grid # true policy (c_star)
+verify_true_policy(m, shocks, c_star)
+
+function check_convergence(m, shocks, c_star, g_init; n_iter = 150)
+    @unpack grid, β, ∂u∂c, f, f′ = m
+    g = g_init;
+    plot(m.grid, g, lw = 2, alpha = 0.36, label = L"intial condition $c(y) = y$")
+    for i in 1:n_iter
+        new_g = K(g, grid, β, ∂u∂c, f, f′, shocks)
+        g = new_g
+        plot!(grid, g, lw = 2, alpha = 0.36, label = "")
+    end
+    plot!(grid, c_star, color = :black, lw = 2, alpha = 0.8,
+          label = L"true policy function $c^*$")
+    plot!(legend = :topleft)
+end
+
+check_convergence(m, shocks, c_star, m.grid, n_iter = 15)
+
+
+
+
+##
+
+function K!(Kg, g, grid, β, ∂u∂c, f, fk′, fn', shocks)
+    # This function requires the container of the output value as argument Kg
+    
+        # Construct linear interpolation object
+        g_func = LinearInterpolation(grid, g, extrapolation_bc=Line())
+    
+        # solve for updated consumption value
+        for (i, y) in enumerate(grid)
+            function h(c)
+                vals = ∂u∂c.(g_func.(f(y - c) * shocks)) .* f′(y - c) .* shocks
+                return ∂u∂c(c) - β * mean(vals)
+            end
+            Kg[i] = find_zero(h, (1e-10, y - 1e-10))
+        end
+        return Kg
+    end
+    
+    # The following function does NOT require the container of the output value as argument
+    K(g, grid, β, ∂u∂c, f, fk′, fn', shocks) =
+        K!(similar(g), g, grid, β, ∂u∂c, f, f′, fk′, fn', shocks)
+
+##
+
+isoelastic(c, γ) = isone(γ) ? log(c) : (c^(1 - γ) - 1) / (1 - γ)
+Model = @with_kw (α = 0.65,                            # Productivity parameter
+                  β = 0.95,                            # Discount factor
+                  γ = 1.0,                             # Risk aversion
+                  μ = 0.0,                             # First parameter in lognorm(μ, σ)
+                  s = 0.1,                             # Second parameter in lognorm(μ, σ)
+                  grid = range(1e-6, 4, length = 200), # Grid
+                  grid_min = 1e-6,                     # Smallest grid point
+                  grid_max = 4.0,                      # Largest grid point
+                  grid_size = 200,                     # Number of grid points
+                  u = (c, γ = γ) -> isoelastic(c, γ),  # utility function
+                  ∂u∂c = c -> c^(-γ),                  # u′
+                  f = (k,n) -> k^α * n^(1-α),          # production function
+                  fk′ = k -> α * k^(α - 1),             # f′ wrt to k
+                  fn′ = n -> (1-α) * (k/n)^α,             # f′ wrt to n
+                  )
+
+##
+
+m = Model()
